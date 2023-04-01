ce01244aff5a12996e176ee06e4eea621be9a658
Ylexis Butler <ylexislauren@gmail.com>
2023-03-31 21:12:04 -0400

    madeQuoteMachine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [hex, setHex] = useState("#000000");
+
+  // eslint-disable-next-line no-undef
+  const randomizedHex = () => {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+    setHex(randomColor);
+  };
+
+  const quoteAPI = async () => {
+    let arrayOfQuotes = [];
+    try {
+      const data = await axios.get("https://api.quotable.io/random");
+
+      arrayOfQuotes = data.data;
+    } catch (error) {
+      console.log(error);
+    }
+    try {
+      setQuote(arrayOfQuotes.content);
+      setAuthor(arrayOfQuotes.author);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    quoteAPI();
+  }, []);
+  document.body.style.backgroundColor = `${hex}`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div id="quote-box" className="fade-in">
+      <div className="container">
+        <h1 style={{ color: `${hex}` }} id="text">
+          "{quote}"
+        </h1>
+
+        <p style={{ color: `${hex}` }} id="author">
+          -{author}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      </div>
+      <button
+        style={{ backgroundColor: `${hex}` }}
+        id="new-quote"
+        onClick={() => {
+          // eslint-disable-next-line no-undef
+          randomizedHex();
+          quoteAPI();
+        }}
+      >
+        New Quote
+      </button>
+      <div className="socials">
+        <a id="tweet-quote" href="twitter.com/intent/tweet">
+          <img
+            id="twitter"
+            style={{ backgroundColor: `${hex}` }}
+            src="./images/twitter.png"
+            alt=""
+          />
         </a>
-      </header>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
